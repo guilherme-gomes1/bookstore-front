975018d34fc55c15c767c070134a473064c140f8
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-17 11:37:24 -0300

    metodo read para o livro implementado

---
+++ src/app/components/views/livro/livro-read/livro-read.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivroReadComponent } from './livro-read.component';
+
+describe('LivroReadComponent', () => {
+  let component: LivroReadComponent;
+  let fixture: ComponentFixture<LivroReadComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LivroReadComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LivroReadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
