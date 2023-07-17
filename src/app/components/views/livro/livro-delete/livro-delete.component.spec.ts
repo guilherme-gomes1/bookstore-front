5f56edf741242a906acaacffd552a2cf13f061d5
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-17 11:04:10 -0300

    metodo delete livro implementado

---
+++ src/app/components/views/livro/livro-delete/livro-delete.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivroDeleteComponent } from './livro-delete.component';
+
+describe('LivroDeleteComponent', () => {
+  let component: LivroDeleteComponent;
+  let fixture: ComponentFixture<LivroDeleteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LivroDeleteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LivroDeleteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
